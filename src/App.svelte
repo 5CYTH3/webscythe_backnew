3c6e16793b89ad0fe62f82aab3d37452c923d829
5CYTH3 <ScytheD@outlook.fr>
2021-08-04 18:32:10 +0200

    🎐 Added routing function

--- src/App.svelte
+++ src/App.svelte
@@ -1,74 +1,33 @@
 <script>
-import Navigation from "./components/Navigation.svelte";
+    import { Router, Link, Route } from "svelte-routing";
+	import Contact from "./routes/Contact.svelte";
+	import Skills from "./routes/Skills.svelte";
+	import Home from "./routes/Home.svelte"
+
+    export let url = "";
 
 </script>
+
 <main>
-	<section>
-		<div class="main__container">
-			<div class="text__container">
-				<h2>Hi, I'm <span>Lucas</span></h2>
-				<p>a young french Webdev based
-					<br>in Brittany, in France.</p>
-				<button>About</button>  
-			</div>
-			<img src="res/img/blobchibi.png" alt="">
-		</div>
-	</section>
-	<section class="section__2">
-		<div class="title__container">
-			<h2>About me</h2>
-		</div>
-		<div class="main__second">
-			<div class="para__container">
-				<p>At the age of ten, I started to
-					<br>discover the programmation with Java and Spigot. 
-					<br>Two years later, I decided to put Spigot away and
-					<br>improve my "pure" Java mastery. I also learnt the 
-					<br>basics of python, which opened new doors of 
-					<br>possibilities for me. At the age of fourteen, I
-					<br>started to learn the front-end with HTML, CSS and 
-					<br>then SASS. Actually, I am trying Svelte.js and I'm a
-					<br>Flutter enthusiast.
-				</p>
-			</div>
-			<img src="./res/img/dev_activity.svg" alt="">
-		</div>
-	</section>
-	<section class="section__3">
-		<div class="title__container">
-			<h2>Latest projects</h2>
-		</div>
-		<div class="grid">
-			<div class="grid__item">
-				<div class="image__wrapper1"></div>
-				<h3>The Tasko app</h3>
-				<p>This app allows you to create task faster and easier than others. It is made in Flutter.</p>
-				<a href="https://github.com/5CYTH3/tasko_rem" target="blank">Learn more</a>
-			</div>
-			<div class="grid__item">
-				<div class="image__wrapper2"></div>
-				<h3>Le Domaine Nantais</h3>
-				<p>"Le Domaine Nantais" is a startup which provide rooms for weddings or meetings.</p>
-				<a href="https://domaine-nantais.com/" target="blank">Learn more</a>
-			</div>
-			<div class="grid__item">
-				<div class="image__wrapper3"></div>
-				<h3>Le centre de la danse</h3>
-				<p>This website has been a lot modified after I leave
-					<br>but the foundations are still here !</p>
-				<a href="https://lecentredeladanse.com/" target="blank">Learn more</a>
-			</div>
-		</div>
-	</section>
-	<section>
-		<!-- Create languages section (for languages that i speak and master) -->
+	<Router url="{url}">
+		<header id="header">
+				<nav id="navbar" class="container">
+					<Link to="Home" href="index.html"><h1>Scythe</h1></Link>
+					<div class="nav__menu">
+						<ul class="nav__list" id="nav-list">
+							<li><Link to="Contact" href="#" class="nav__link"><i class="uil uil-diary"></i>Contact</Link></li>
+							<li><Link to="Skills" href="#" class="nav__link"><i class="uil uil-clipboard-alt"></i>Skills</Link></li>
+							<li><a href="https://github.com/5CYTH3" class="nav__link"><i class="uil uil-github-alt"></i>Github</a></li>
+						</ul>
+					<i class="uil uil-bars"></i>
+					</div> 
+				</nav>
+			
+		</header>	
 		<div>
-
+			<Route path="Home" component="{Home}" />
+			<Route path="Contact" component="{Contact}" />
+			<Route path="Skills" component="{Skills}" />
 		</div>
-
-	</section>
-	
-	<footer>
-		
-	</footer>
+	</Router>
 </main>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,68 @@
+<section>
+    <div class="main__container">
+        <div class="text__container">
+            <h2>Hi, I'm <span>Lucas</span></h2>
+            <p>a young french Webdev based
+                <br>in Brittany, in France.</p>
+            <button>About</button>  
+        </div>
+        <img src="res/img/blobchibi.png" alt="">
+    </div>
+</section>
+<section class="section__2">
+    <div class="title__container">
+        <h2>About me</h2>
+    </div>
+    <div class="main__second">
+        <div class="para__container">
+            <p>At the age of ten, I started to
+                <br>discover the programmation with Java and Spigot. 
+                <br>Two years later, I decided to put Spigot away and
+                <br>improve my "pure" Java mastery. I also learnt the 
+                <br>basics of python, which opened new doors of 
+                <br>possibilities for me. At the age of fourteen, I
+                <br>started to learn the front-end with HTML, CSS and 
+                <br>then SASS. Actually, I am trying Svelte.js and I'm a
+                <br>Flutter enthusiast.
+            </p>
+        </div>
+        <img src="./res/img/dev_activity.svg" alt="">
+    </div>
+</section>
+<section class="section__3">
+    <div class="title__container">
+        <h2>Latest projects</h2>
+    </div>
+    <div class="grid">
+        <div class="grid__item">
+            <div class="image__wrapper1"></div>
+            <h3>The Tasko app</h3>
+            <p>This app allows you to create task faster and easier than others. It is made in Flutter.</p>
+            <a href="https://github.com/5CYTH3/tasko_rem" target="blank">Learn more</a>
+        </div>
+        <div class="grid__item">
+            <div class="image__wrapper2"></div>
+            <h3>Le Domaine Nantais</h3>
+            <p>"Le Domaine Nantais" is a startup which provide rooms for weddings or meetings.</p>
+            <a href="https://domaine-nantais.com/" target="blank">Learn more</a>
+        </div>
+        <div class="grid__item">
+            <div class="image__wrapper3"></div>
+            <h3>Le centre de la danse</h3>
+            <p>This website has been a lot modified after I leave
+                <br>but the foundations are still here !</p>
+            <a href="https://lecentredeladanse.com/" target="blank">Learn more</a>
+        </div>
+    </div>
+</section>
+<section>
+    <!-- Create languages section (for languages that i speak and master) -->
+    <div>
+
+    </div>
+
+</section>
+
+<footer>
+    
+</footer>
