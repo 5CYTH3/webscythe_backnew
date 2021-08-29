8e1c30fd6e4f1d128707767ba69721e020163fb3
5CYTH3 <ScytheD@outlook.fr>
2021-08-29 15:24:53 +0200

    Added contact

---
+++ src/routes/Contact.svelte
@@ -0,0 +1,17 @@
+<section class="section__1b">
+    <div class="title__separator">
+        <h1></h1>
+    </div>
+    <div class="sep__te">
+        <div class="sep__left">
+            <div>
+                <p><i class="uil uil-instagram"></i>Instagram - @scythe</p>
+                <p><i class="uil uil-envelope"></i>eMail - ScytheD@outlook.fr</p>
+                <p><i class="uil uil-twitter-alt"></i>Twitter - @Scythe</p>
+                <p><i class="uil uil-github-alt"></i>Github - 5CYTH3</p>
+                <p><i class="uil uil-discord"></i>Discord - @Scythe#0356</p>
+            </div>
+        </div>
+        <img src="/res/img/illustrations/messaging.svg" alt="">
+    </div>
+</section>
