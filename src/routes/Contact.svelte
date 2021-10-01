9962456b5e8880ed4ee6b970702e1e9d88493202
5CYTH3 <ScytheD@outlook.fr>
2021-10-01 20:06:52 +0200

    🍣 All project and styles migrated to SvelteKit.

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -5,7 +5,6 @@
     <div class="sep__te">
         <div class="sep__left">
             <div>
-
                 <p><i class="uil uil-instagram"></i>Instagram - @scythe_crea</p>
                 <p><i class="uil uil-envelope"></i>eMail - ScytheD@outlook.fr</p>
                 <p><i class="uil uil-twitter-alt"></i>Twitter - @Scythe</p>
@@ -13,6 +12,10 @@
                 <p><i class="uil uil-discord"></i>Discord - @Scythe#0356</p>
             </div>
         </div>
-        <img src="/res/img/illustrations/messaging.svg" alt="">
+        <img src="/img/illustrations/messaging.svg" alt="">
     </div>
 </section>
+
+<style lang="scss" global>
+    @import '../sass/views/contact.scss';
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,18 @@
 <script>
+    import Footer from "../components/Footer.svelte";
     import Navbar from "../components/Navbar.svelte";
 </script>
+
+<svelte:head>
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+</svelte:head>
 
 <Navbar />
 <main>
     <slot></slot>
 </main>
+<Footer />
+
+<style lang="scss" global>
+    @import '../sass/style.scss';
+</style>
